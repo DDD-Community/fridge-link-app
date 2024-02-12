71f68b389ef59e98eaf969690977d2da9ee5820c
한혜선 <hyeseon405@gmail.com>
2024-02-12 15:23:43 +0900

    feat: 안드로이드 스플래시 기능 추가  (#2)
    * chore: react-native-splash-screen install
    
    * feat: splash 추가

--- android/app/src/main/java/com/maraapp/MainActivity.kt
+++ android/app/src/main/java/com/maraapp/MainActivity.kt
@@ -5,6 +5,12 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+import android.os.Bundle
+
+import org.devio.rn.splashscreen.SplashScreen
+
+
+
 class MainActivity : ReactActivity() {
 
   /**
@@ -19,4 +25,12 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  /** 
+   * splash  
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this)
+    super.onCreate(savedInstanceState)
+  }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import React, {useEffect} from 'react';
+
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
 import RootStack from '@screens';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+	useEffect(() => {
+		setTimeout(() => {
+			SplashScreen.hide();
+		}, 2000);
+	}, []);
+
 	return (
 		<SafeAreaProvider>
 			<NavigationContainer>
