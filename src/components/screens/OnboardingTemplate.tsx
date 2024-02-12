51f50ccb08f28ea44d3dab780b9c575ae297a23a
hyeseon han <hyeseon405@gmail.com>
2024-02-12 14:02:33 +0900

    fix: OnboardingTemplate 하단 button 분기

--- src/components/screens/OnboardingTemplate.tsx
+++ src/components/screens/OnboardingTemplate.tsx
@@ -1,30 +1,32 @@
-import { Button, StepLabel } from '@components/common';
+import {Button, StepLabel} from '@components/common';
 import {Image, StyleSheet, View} from 'react-native';
 
 import COLOR from '@utils/theme/colors';
 import React from 'react';
 import {STATIC_IMG_URI} from '@utils/constants';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
 const ONBOARDING_TOTAL_PAGE = 3;
 
-const OnboardingTemplate:React.FC<{onNextStep: () => void, curPage: number}> = ({onNextStep, curPage}) => {
-  const navigation  = useNavigation<any>();
+const OnboardingTemplate: React.FC<{onNextStep: () => void; curPage: number}> = ({onNextStep, curPage}) => {
+	const navigation = useNavigation<any>();
 
-  return (
-    <View style={styles.container}>
-    <StepLabel curPage={curPage} totalPage={ONBOARDING_TOTAL_PAGE} />
-    <Image style={styles.image} source={{uri: `${STATIC_IMG_URI}/img_onboarding_${curPage}.png`}} />
-    <View style={styles.buttonWrapper}>
-      <Button variant={'clickable'} label={'다음'} onPress={onNextStep} />
-      {ONBOARDING_TOTAL_PAGE > curPage ?
-        <Button variant={'skippable'} label={'건너뛰기'} onPress={() => navigation.navigate('Main')} />
-        :
-        <View style={styles.emptyButton} />
-    }
-    </View>
-  </View>
-  );
+	return (
+		<View style={styles.container}>
+			<StepLabel curPage={curPage} totalPage={ONBOARDING_TOTAL_PAGE} />
+			<Image style={styles.image} source={{uri: `${STATIC_IMG_URI}/img_onboarding_${curPage}.png`}} />
+			<View style={styles.buttonWrapper}>
+				{ONBOARDING_TOTAL_PAGE > curPage ? (
+					<>
+						<Button variant={'clickable'} label={'다음'} onPress={onNextStep} />
+						<Button variant={'skippable'} label={'건너뛰기'} onPress={() => navigation.navigate('Main')} />
+					</>
+				) : (
+					<Button variant={'clickable'} label={'시작하기'} onPress={onNextStep} />
+				)}
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
@@ -42,9 +44,6 @@ const styles = StyleSheet.create({
 		aspectRatio: 320 / 529,
 		width: '85%',
 	},
-  emptyButton:{
-    height: 51
-  }
 });
 
 export default OnboardingTemplate;
